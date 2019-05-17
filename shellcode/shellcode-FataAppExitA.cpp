edbbdca5e1cb4a802a79ec1cf65060199289d2ef
yusakul <32380039+yusakul@users.noreply.github.com>
2019-05-17 11:53:14 +0800

    Update shellcode-FataAppExitA.cpp

--- shellcode/shellcode-FataAppExitA.cpp
+++ shellcode/shellcode-FataAppExitA.cpp
@@ -16,3 +16,53 @@ int main(int argc, char **argv)
 	f = (int(*)())shellcode; 
 	(int)(*f)(); 
 }
+
+
+
+void ShellCodeFunction()
+{
+	typedef void(__stdcall *Pfn_FatalAppExit)(UINT, LPCSTR);
+	Pfn_FatalAppExit pfnFatalAppExit = NULL;
+
+	PCHAR pBuf = NULL;
+	_asm
+	{
+		mov eax, fs:[0x30];//找到PEB
+		mov eax, [eax + 0x0C];//找到了LDR
+		mov eax, [eax + 0x0C];//找到了第一个节点
+		mov eax, [eax];       //找到了ntdll
+		mov eax, [eax];       //找到了kernel32.dll
+		mov ebx, dword ptr ds : [eax + 0x18];	//在0x18偏移处取出基址(DllBase)
+		mov pBuf, ebx;
+	}
+
+	//kernel32.dll 解析
+	PIMAGE_DOS_HEADER pDos = (PIMAGE_DOS_HEADER)pBuf;	//kernel32.dll的dos头
+	PIMAGE_NT_HEADERS pNt = (PIMAGE_NT_HEADERS)(pDos->e_lfanew + pBuf);
+
+	PIMAGE_DATA_DIRECTORY pExportDir =
+		(pNt->OptionalHeader.DataDirectory + 0);
+
+	PIMAGE_EXPORT_DIRECTORY pExport = (PIMAGE_EXPORT_DIRECTORY)
+		(pExportDir->VirtualAddress + pBuf);
+	//后面的步骤
+
+	//1  找到三个表：名称，地址，序号
+	PDWORD pAddress = (PDWORD)(pExport->AddressOfFunctions + pBuf);
+	PDWORD pName = (PDWORD)(pExport->AddressOfNames + pBuf);
+	PWORD  pId = (PWORD)(pExport->AddressOfNameOrdinals + pBuf);
+	PVOID Address = 0;
+
+	//2  在名称表中去遍历GetProcAddress这个字符串
+	for (size_t i = 0; i < pExport->NumberOfNames; i++)
+	{
+		char* Name = (pName[i] + pBuf);
+		if (strcmp(Name, "FatalAppExitA") == 0)
+		{
+			Address = pAddress[pId[i]] + pBuf;
+		}
+	}
+	pfnFatalAppExit = (Pfn_FatalAppExit)Address;
+
+	pfnFatalAppExit(0, "by Yusakul");
+}
