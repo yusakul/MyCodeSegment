3bba12e14281103a05c7b6a90cc9f33fad40f86d
yusakul <32380039+yusakul@users.noreply.github.com>
2022-07-01 15:13:07 +0800

    Update VMDetected.ps1

--- WMI/VMDetected.ps1
+++ WMI/VMDetected.ps1
@@ -1,7 +1,14 @@
 $VMDetected=$False
 $Arguments= @{
- Class ="Win32_ComputerSystem"
- Filter ="NumberOfLogicalProcessors < 2 OR TotalPhysicalMemory < 2147483648"
+ Class = 'Win32_ComputerSystem'
+ Filter = 'NumberOfLogicalProcessors < 2 OR TotalPhysicalMemory < 2147483648'
 }
 if (Get-WmiObject @Arguments) { $VMDetected=$True }
 $VMDetected
+
+$VMwareDetected = $False
+$VMAdapter = Get-WmiObject Win32_NetworkAdapter -Filter 'Manufacturer LIKE "%VMware%" OR Name LIKE "%VMware%"'
+$VMBios = Get-WmiObject Win32_BIOS -Filter 'SerialNumber LIKE "%VMware%"'
+$VMToolsRunning = Get-WmiObject Win32_Process -Filter 'Name="vmtoolsd.exe"'
+if ($VMAdapter -or $VMBios -or $VMToolsRunning) { $VMwareDetected = $True }
+$VMwareDetected
